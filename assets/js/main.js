f8af666df1e4d4e0ccfc6d56567e4cf03e778342
Chris-Armel <info@daqhris.com>
2023-01-28 00:14:07 +0100

    Replace max-width value 480px by 320px

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 600px)',
-		xsmall: '(max-width: 480px)'
+		xsmall: '(max-width: 320px)'
 	});
 
 	$(function() {
