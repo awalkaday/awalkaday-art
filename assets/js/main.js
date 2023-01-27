d9a5c2b0b7dc681392e621c57136187d9d1e55b0
Chris-Armel <info@daqhris.com>
2023-01-27 17:45:30 +0100

    Change Skel library breakpoints
    Implementing changes recommended by ChatGPT. 
    https://twitter.com/awalkadayart/status/1619012171610075136
    
    To display at least two vertical rows on a smartphone, I modified the small breakpoints to have a smaller max-width value. 
    This will set the small breakpoints to be triggered when the screen width is less than 600px, which should display two or more rows on most smartphones.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@
 
 (function($) {
 
-	skel.breakpoints({
+		skel.breakpoints({
 		xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
+		small: '(max-width: 600px)',
 		xsmall: '(max-width: 480px)'
 	});
 
